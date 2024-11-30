6aa86a12d80b3029a0edd848d284d50f50f1f30d
Mingcan Yang <83692733+MingcanYang@users.noreply.github.com>
2024-12-01 02:53:29 +1100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+/*
+Copyright (c) 2024 Mingcan Yang
+Licensed under the MIT License. See LICENSE file for details.
+*/
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
